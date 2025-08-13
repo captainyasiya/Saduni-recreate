2e79e179b68cc36be3a7ff1de1031f55b962a4f3
captainyasiya <ryasiru5@gmail.com>
2025-08-13 22:03:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ async function connectToWA() {
     } else if (connection === 'open') {
       console.log('✅ සදුනි-MD connected to WhatsApp');
 
-      const up = `සදුනි-MD connected ✅\n\n PREFIX: ${prefix}`;
+      const up = `සදුනි-MD connected ✅\n\nPREFIX: ${prefix}`;
       await saduni.sendMessage(ownerNumber[0] + "@s.whatsapp.net", {
         image: { url: `https://github.com/captainyasiya/Saduni-recreate/blob/main/images/SADUNI%20-%20MD%202.0.png?raw=true` },
         caption: up
