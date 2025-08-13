3b5751ffa37f519bf5785f2735a1aed2dba199ef
captainyasiya <ryasiru5@gmail.com>
2025-08-13 19:20:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/captainyasiya/Saduni-recreate/blob/main/images/SADUNI%20-%20MD%20(1)alive.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 සදුනි Is Alive Now😍*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+BOT_OWNER: '94765683261',  // Replace with the owner's phone number
 
 
 
