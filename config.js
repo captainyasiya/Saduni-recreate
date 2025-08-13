52e62cab69b0288c631d4311838f5aeb065a677c
captainyasiya <ryasiru5@gmail.com>
2025-08-13 19:42:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "VEAjETYA#1AFP0bjwnMWAxPsL71NwVPbC04fG6Xmgu3EP1RD0daM",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/captainyasiya/Saduni-recreate/blob/main/images/SADUNI%20-%20MD%20(1)alive.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 සදුනි Is Alive Now😍*",
 BOT_OWNER: '94765683261',  // Replace with the owner's phone number
